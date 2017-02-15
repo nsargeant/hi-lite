82c3f57dd666df247a117640bfb80e41a17faddb
Nathan Sargeant <nathan.sargeant@ldschurch.org>
2017-02-14 19:47:10 -0700

    Added more tests and fixed highlight

--- test/highlight-test.js
+++ test/highlight-test.js
@@ -16,6 +16,20 @@ describe('highlight', () => {
     assert.deepEqual(result, '<p>this is a <mark>test</mark></p>');
   })
 
+  it('should highlight first and last', () => {
+    const query = 'highlight';
+    const html = `<p>highlight text highlight</p>`
+    const result = highlight(query, html);
+    assert.deepEqual(result, `<p><mark>highlight</mark> text <mark>highlight</mark></p>`);
+  })
+
+  it('should highlight simple middle', () => {
+    const query = 'highlight';
+    const html = `<p>text highlight text</p>`
+    const result = highlight(query, html);
+    assert.deepEqual(result, `<p>text <mark>highlight</mark> text</p>`);
+  })
+
   it('should highlight "a lot of tests"', () => {
     const query = 'a lot of tests';
     const html = `<p>there are a <em>lot</em> of tests man</p>`;
@@ -36,4 +50,33 @@ describe('highlight', () => {
     const result = highlight(query, html);
     assert.deepEqual(result, '<p>this is <mark>a </mark><b><mark>test</mark> suite</b></p><p>this is <mark>a </mark><b><mark>test</mark> suite</b></p>');
   })
+
+  it('should highlight nested content that matches "what about bob"', () => {
+    const query = 'what about bob';
+    const html = `<div>Movie: <p><b>What</b> about Bob?</p></div>`;
+    const result = highlight(query, html);
+    assert.deepEqual(result, '<div>Movie: <p><b><mark>What</mark></b><mark> about Bob</mark>?</p></div>');
+  })
+
+  it('should ignore script', () => {
+    const query = 'var';
+    const html = `<div><script>var thing = 'testing'</script></div>`;
+    const result = highlight(query, html);
+    assert.deepEqual(result, `<div><script>var thing = 'testing'</script></div>`);
+  })
+
+  it('should handle deeply nested', () => {
+    const query = 'abbott';
+    const html = `<ul class="reference-index__index-columns"><li><a href="#">Abbott, Hiram</a></li><li><a href="#">Abbott, Lewis</a></li></ul>`
+    const result = highlight(query, html);
+    assert.deepEqual(result, `<ul class="reference-index__index-columns"><li><a href="#"><mark>Abbott</mark>, Hiram</a></li><li><a href="#"><mark>Abbott</mark>, Lewis</a></li></ul>`);
+  })
+
+  it.skip('should handle &nbsp;', () => {
+    const query = 'broke it';
+    const html = `<p>I broke <span>&nbsp;</span>it for</p>`;
+    const result = highlight(query, html);
+    assert.deepEqual(result, `<p>I <mark>broke</mark> <span>&nbsp;</span><mark>it</mark> for</p>`);
+  })
+
 });
